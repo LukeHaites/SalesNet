47294066ac0efcd46ce84e0fb1180dbbf83027c6
Luke Haites <Luke.Haites@Apparel21.com>
2013-09-19 18:45:57 +1000

    Formatting

--- SalesNet/Scripts/site.js
+++ SalesNet/Scripts/site.js
@@ -1,9 +1,5 @@
 ﻿$(document).ready(function () {
   $("body").on("click", ".showdets", function () {
-    $(this).closest("tr").next("tr").find("div").slideDown(200);
-  });
-
-  $("body").on("click", ".hidedets", function () {
-    $(this).closest("tr").find("div").slideUp(200);
+    $(this).closest("tr").next("tr").find("div").slideToggle(200);
   });
 });

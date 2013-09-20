563683737e1df0372d1dcf55f7f3b6cb7212a803
LukeHaites <Luke.Haites@gmail.com>
2013-09-20 18:38:20 +1000

    Drill down arrow

--- SalesNet/Scripts/site.js
+++ SalesNet/Scripts/site.js
@@ -1,5 +1,7 @@
 ﻿$(document).ready(function () {
   $("body").on("click", ".showdets", function () {
-    $(this).closest("tr").next("tr").find("div").slideToggle(200);
+      $(this).closest("tr").next("tr").find("div").slideToggle(200);
+      $(this).toggleClass("ui-icon-triangle-1-s");
+      $(this).toggleClass("ui-icon-triangle-1-n");
   });
 });
